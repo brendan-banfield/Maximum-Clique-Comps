1edb59282b1d75d0ebd90b051baf32d81c2e8674
brendan-banfield <banfieldb@carleton.edu>
2025-01-24 12:05:24 -0600

    Fixed incorrectly pushed version of simulated annealing algorithm. This file can be used as a blueprint for implementing new algorithms

--- simulatedAnnealing.py
+++ simulatedAnnealing.py
@@ -2,49 +2,84 @@ from graphs import Graph
 import random
 import math
 
-def simulated_annealing(graph: Graph, k: int, T_0: float, T_f: float, alpha: float) -> list[int]:
-    permutation = list(range(graph.vertices))
-    # sort by degree
-    adjecency_list = graph.get_adj_list()
-    permutation.sort(key=lambda x: len(adjecency_list[x]), reverse=True)
+class Simulated_Annealing_Solver:
+    def __init__(self, graph: Graph, k: int, T_0: float, T_f: float, alpha: float, visualize: bool = False):
+        self.graph = graph
+        self.k = k
+        self.T_0 = T_0
+        self.T_f = T_f
+        self.T = T_0
+        self.alpha = alpha
+        self.completed = False
+        self.succeeded = False
+        self.setup()
 
-    adjecency_matrix = graph.get_adj_matrix()
-
-    def connectedness_delta(v1, v2):
-        v1_connectedness = 0
-        for v in range(k):
-            if v1 != v and adjecency_matrix[permutation[v1]][permutation[v]]:
-                v1_connectedness += 1
-        v2_connectedness = 0
-        for v in range(k):
-            if v1 != v and adjecency_matrix[permutation[v2]][permutation[v]]:
-                v2_connectedness += 1
-        return v2_connectedness - v1_connectedness
+    def connectedness_delta(self, v1, v2):
+        # v1_connectedness = sum([1 for v in range(k) if v1 != v and adjecency_matrix[permutation[v1]][permutation[v]]])
+        # v2_connectedness = sum([1 for v in range(k) if v1 != v and adjecency_matrix[permutation[v2]][permutation[v]]])
+        # return v2_connectedness - v1_connectedness
+        # the 1 liner below does that ^
     
-    def is_clique():
-        return sum([1 for i in range(k-1) for j in range(i + 1, k) if not adjecency_matrix[permutation[i]][permutation[j]]])
-    
-    T = T_0
-    max_occurences = 8 * graph.vertices
-    best_score = graph.vertices ** 2
-    while T > T_f:
+        return sum([self.adjecency_matrix[self.permutation[v2]][self.permutation[v]] - self.adjecency_matrix[self.permutation[v1]][self.permutation[v]] for v in range(self.k) if v != v1])
 
-        v1 = random.randint(0, k - 1)
-        v2 = random.randint(k, graph.vertices - 1)
-        c_delta = connectedness_delta(v1, v2)
+    def num_missing_edges(self):
+        return sum([1 for i in range(self.k-1) for j in range(i + 1, self.k) if not self.adjecency_matrix[self.permutation[i]][self.permutation[j]]])
+
+    def setup(self):
+        self.permutation = list(range(self.graph.vertices))
+        self.adjecency_matrix = self.graph.get_adj_matrix()
+        self.adjecency_list = self.graph.get_adj_list()
+
+        # sort by degree
+        self.permutation.sort(key=lambda x: len(self.adjecency_list[x]), reverse=True)
+        self.running_clique_score = self.num_missing_edges()
+        self.best_score = self.running_clique_score
+        self.best_nodes = self.permutation[:self.k]
+        if self.running_clique_score == 0:
+            self.completed = True
+            self.succeeded = True
         
-        if c_delta > 0 or random.random() < math.exp(c_delta / T):
-            permutation[v1], permutation[v2] = permutation[v2], permutation[v1]
-            clique_score = is_clique()
-            if clique_score == 0:
-                return permutation[:k]
-            if clique_score < best_score:
-                best_score = clique_score
-        T *= alpha
-    return None
+    def update(self):
+        if self.completed:
+            return
+        v1 = random.randint(0, self.k - 1)
+        v2 = random.randint(self.k, self.graph.vertices - 1)
+        c_delta = self.connectedness_delta(v1, v2)
+        
+        if c_delta > 0 or random.random() < math.exp(c_delta / self.T):
+            self.permutation[v1], self.permutation[v2] = self.permutation[v2], self.permutation[v1]
+            self.running_clique_score -= c_delta
+            if self.running_clique_score < self.best_score:
+                self.best_score = self.running_clique_score
+                self.best_nodes = self.permutation[:self.k]
+            if self.running_clique_score == 0:
+                self.completed = True
+                self.succeeded = True
+                return
+                # return f"Succeeded: {permutation[:k]}"
+            
+        self.T *= self.alpha
+        if self.T < self.T_f:
+            self.completed = True
+
+    def current_active_nodes(self):
+        return self.permutation[:self.k]
+    
+    def is_completed(self):
+        return self.completed
+        
+
+    def run(self):
+        while not self.completed:
+            self.update()
+        return f"Succeeded: {self.succeeded}, best score: {self.best_score}"
 
     
 
-graph = Graph.get_graph_from_dataset('c-fat200-2')
-for i in range(20):
-    print(simulated_annealing(graph, 24, 100, 0.001, 0.9996))
+graph = Graph.get_graph_from_dataset('C250.9')
+solver = Simulated_Annealing_Solver(graph, 44, 100, 0.001, 0.9998) 
+graph.visualize_algorithm(solver, 0.01)
+# for i in range(20):
+#     solver = Simulated_Annealing_Solver(graph, 44, 100, 0.001, 0.9998) 
+#     print(solver.run())
+#     # print(simulated_annealing(graph, 44, 100, 0.001, 0.9998))
