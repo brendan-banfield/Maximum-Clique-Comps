f2b00710bba0052c7f7054b5b500657add3f5de5
quinns7 <quinns@carleton.edu>
2025-02-02 15:48:01 -0600

    finished Bron-Kerbosch no pivot

--- bronKerbosch.py
+++ bronKerbosch.py
@@ -2,62 +2,68 @@ from graphs import Graph
 
 class Bron_Kerbosch_Solver:
     def __init__(self, graph: Graph):
-        # self.compsub_R = [] -- implement this as global?
         self.graph = graph
         self.adjacencyList = self.graph.get_adj_list()
-        #print(self.adjacencyList)
         self.maximal_cliques = []
+        self.succeeded = False
 
-
+    '''solves Bron Kerbosch without a pivot vertex'''
     def no_pivot_solver(self, candidates_P, compsub_R, not_X):
-        #print("candidates 1: {0}".format(candidates_P))
         if not candidates_P:
-            #print("i am done for now")
             if not not_X:
                 self.maximal_cliques.append(compsub_R)
             return
         
-        #print("i am above")
         for v in candidates_P:
-            #print("i am in loop and v = {0}".format(v))
-            compsub_R.append(v)
+            new_compsub_R = compsub_R.copy()
+            new_compsub_R.append(v)
 
-            new_candidates_P = [] #intersection nbrs(v). see below
+            new_candidates_P = [] 
             for node in candidates_P:
                 if node in self.adjacencyList[v]:
                     new_candidates_P.append(node)
             
-            new_not_X = [] #intersection nbrs(v). see below
-            for node in not_X:
-                if node in self.adjacencyList[v]:
-                    new_not_X.append(node)
-            # print("candidates: " + new_candidates_P)
-            # print("compsub: " + compsub_R)
-            # print("not: " + new_not_X)
-            #print("candidates before: {0}".format(candidates_P))
-            self.no_pivot_solver(new_candidates_P, compsub_R, new_not_X)
-            #print("v: {0}".format(v))
-            #print("candidates after: {0}".format(candidates_P))
+            new_not_X = [] 
+            if not_X:
+                for node in not_X:
+                    if node in self.adjacencyList[v]:
+                        new_not_X.append(node)
+            self.no_pivot_solver(new_candidates_P, new_compsub_R, new_not_X)
             candidates_P.remove(v)
             not_X.append(v)
     
-    def get_maximum_clique():
-        pass
+    '''finds the maximum clique among the maximal cliques produced'''
+    def get_maximum_clique(self):
+        if self.maximal_cliques:
+            max_clique_val = len(self.maximal_cliques[0])
+            max_clique_index = 0
+            for i in range(1, len(self.maximal_cliques)):
+                value = len(self.maximal_cliques[i])
+                if value > max_clique_val:
+                    max_clique_index = i
+                    max_clique_val = value
+            self.succeeded = True
+        return max_clique_val
+
+    def found_clique(self):
+        return self.succeeded
 
     def run(self):
         candidates = [i for i in range(self.graph.vertices)]
-        #print(candidates)
         compsub = []
         not_x = []
         self.no_pivot_solver(candidates, compsub, not_x)
-        #return biggest clique
+        return self.get_maximum_clique()
 
 
 if __name__ == "__main__":
-    graph = Graph.get_graph_from_dataset('C250.9')
+    baby_graph = Graph(5, [(0, 1), (1, 2), (0, 2), (3, 1), (3, 4)])
+    graph = Graph(7, [(0,1), (0,2), (0,3), (1,2), (1,3), (2,3), (1,4), (3,4), (4,6), (3,5)])
     solver = Bron_Kerbosch_Solver(graph)
-    solver.run()
+    print(solver.run())
     print(solver.maximal_cliques)
+    Graph.test_algorithm(Bron_Kerbosch_Solver)
+    
 
 
     
